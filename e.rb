26be92df60c7047aea486ab14de49a77aa760204
Taylor Bell <taylorbell@gmail.com>
2014-02-27 15:15:17 -0700

    objectified. remove non-breaking spaces. media is array of iframes

--- e.rb
+++ e.rb
@@ -2,10 +2,11 @@ require 'mechanize'
 require 'nokogiri'
 require_relative 'show'
 
-f = File.open("friday.html")
+f = File.open("wednesday.html")
 doc = Nokogiri::HTML(f)
 f.close
 
+agent = Mechanize.new
 link_rows = doc.search("tr")
 
 shows = []
@@ -17,20 +18,29 @@ link_rows.each do |row|
 		(!row.css('.title > a').children.text.include?("[COMEDY]"))
 
 
-		time = row.css('.time').text.gsub(" ", "").delete("\n")
-		artist = row.css('.title > a').children[0].text
-		venue = row.css('.title > a').children[1].text
+		time = row.css('.time').text.gsub(" ", "").delete("\n").gsub(/\302\240/,"")
+		artist = row.css('.title > a').children[0].text.gsub(/\302\240/,"")
+		venue = row.css('.title > a').children[1].text.gsub(/\302\240/,"")
+
+		media = []
 		# link = row.css('.sched-description > a')[0].text		
 		if row.at_css('.sched-description > a') != nil
-			link = row.css('.sched-description > a')[0].text
+			link = row.css('.sched-description > a')[0].text.gsub(/\302\240/,"")
+
+			artist_page = agent.get(link)
+			
+			if artist_page.search(".one-third.last > p > iframe") != nil
+				media << artist_page.search(".one-third.last > p > iframe").to_a
+			end
 		end
 		# puts "#{time} - #{artist} - #{venue} - #{link}"
-		show = Show.new(artist, venue, time, link)
+		
+		show = Show.new(artist, venue, time, media)
 
 		shows << show
 	end
 end
 
 shows.each do |x|
-	puts x.artist
+	puts x.media[0]
 end
