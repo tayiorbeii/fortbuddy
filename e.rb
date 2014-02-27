787b43a45e47c1cbcd5f7d07385afaedeb522b4c
Taylor Bell <taylorbell@gmail.com>
2014-02-27 14:53:56 -0700

    show objects created

--- e.rb
+++ e.rb
@@ -8,11 +8,14 @@ f.close
 
 link_rows = doc.search("tr")
 
+shows = []
 link_rows.each do |row|
 	if (!row.css('.title > a').children.text.include?("FILM FEST")) and
 		(!row.css('.title > a').children.text.include?("HACKFORT")) and
 		(!row.css('.title > a').children.text.include?("YOGAFORT")) and
-		(!row.css('.title > a').children.text.include?("STORYFORT"))
+		(!row.css('.title > a').children.text.include?("STORYFORT")) and
+		(!row.css('.title > a').children.text.include?("[COMEDY]"))
+
 
 		time = row.css('.time').text.gsub(" ", "").delete("\n")
 		artist = row.css('.title > a').children[0].text
@@ -21,7 +24,13 @@ link_rows.each do |row|
 		if row.at_css('.sched-description > a') != nil
 			link = row.css('.sched-description > a')[0].text
 		end
-		puts "#{time} - #{artist} - #{venue} - #{link}"
-	end
+		# puts "#{time} - #{artist} - #{venue} - #{link}"
+		show = Show.new(artist, venue, time, link)
 
+		shows << show
+	end
+end
+
+shows.each do |x|
+	puts x.artist
 end
